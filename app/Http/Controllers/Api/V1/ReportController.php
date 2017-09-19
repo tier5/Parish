063ffccccf30e116b6321922378b0e971ef941db
Tier5 LLC <github@tier5.us>
2017-09-19 20:48:12 +0530

    report modified

--- app/Http/Controllers/Api/V1/ReportController.php
+++ app/Http/Controllers/Api/V1/ReportController.php
@@ -1,6 +1,6 @@
 <?php
 /**
-* PaymentController to get list of payment, submit payment, reject payment.
+* Report Controller to get list of payment, submit payment, reject payment.
 * @param Request $request
 */
 namespace App\Http\Controllers\Api\V1;
@@ -27,6 +27,7 @@ use Log;
 use DateTime;
 use DateInterval;
 use DatePeriod;
+use SoftDeletes;
 
 class ReportController extends Controller {
 
@@ -42,7 +43,7 @@ class ReportController extends Controller {
     
     
     /**
-     * Create a new Parish with Poster
+     * Create a new Report with Parish
      *
      * @param Request $request
      * @return \Illuminate\Http\JsonResponse
@@ -63,7 +64,7 @@ class ReportController extends Controller {
 
             if ($request->has('user_id'))
 
-                $report->parish_id = $request->input('user_id');
+                $parish_id = $request->input('user_id');
             else
                 throw new HttpBadRequestException("User Id is required.");
 
@@ -85,13 +86,27 @@ class ReportController extends Controller {
             else
                 throw new HttpBadRequestException("Year is required.");
             
+            $parish                 = Parish::where('user_id',$parish_id)->whereNull('deleted_at')->first();
 
-            $report->save();
-
+            if($parish) {
+                $checkIfexists      = Report:: where('parish_id',$parish->id)
+                                            -> where('report_month', $request->input('report_month'))
+                                            -> where('report_year', $request->input('report_year'))
+                                            -> count();
+                if($checkIfexists > 0) {
+                    throw new HttpBadRequestException("Report already exists.");
+                } else {
+                    $report->parish_id  = $parish->id;
+                    $report->save(); 
+                }                       
+                
+            } else {
+                throw new HttpBadRequestException("Parish not found.");
+            }
             
             $response = [
-            'status'        => true,
-            'message'       => "Report created successfully."
+                'status'        => true,
+                'message'       => "Report created successfully."
             ];
             $responseCode = 201;
            
@@ -160,15 +175,16 @@ class ReportController extends Controller {
             else
                 throw new HttpBadRequestException("Year is required.");
             
-            $parish             = Parish::where('user_id',$parish_id)->first();
+            $parish             = Parish::where('user_id',$parish_id)->whereNull('deleted_at')->first();
 
             if($parish) {
                 $fetchAllreport     = Report::where('parish_id', $parish->id)
                                     ->where('report_month', $report_month)
                                     ->where('report_year', $report_year)
-                                    ->first();
+                                    ->whereNull('deleted_at')
+                                    ->get();
 
-                if($fetchAllreport) {
+                if(count($fetchAllreport) > 0) {
                     $report         = $fetchAllreport->progress_report;
                 } else {
                     
@@ -224,8 +240,208 @@ class ReportController extends Controller {
             } else {
                throw new HttpBadRequestException("Parish not found.");
             }
+        } catch (HttpBadRequestException $httpBadRequestException) {
+                $response = [
+                    'status'    => false,
+                    'error'     => $httpBadRequestException->getMessage()
+                ];
+                $responseCode = 400;
+        } catch (ClientException $clientException) {
+            DB::rollBack();
+
+            $response = [
+                'status'        => false,
+                'error'         => "Internal server error.",
+                'error_info'    => $clientException->getMessage()
+            ];
+            $responseCode = 500;
+        } catch (Exception $exception) {
+            DB::rollBack();
+
+            Log::error($exception->getMessage());
+
+            $response = [
+                'status'        => false,
+                'error'         => "Internal server error.",
+                'error_info'    => $exception->getMessage()
+            ];
+
+            $responseCode = 500;
+        } finally {
+            DB::commit();
+
+            unset($user);
+            unset($provience);
+        }
+        return response()->json($response, $responseCode);
+    }
+
+    /** Function to get no of week within a week for specified month and year **/
+
+    function getWeekDates($date, $start_date, $end_date,$attendance,$monetary){
+
+        $week   =  date('W', strtotime($date));
+        $year   =  date('Y', strtotime($date));
+        $from   = date("Y-m-d", strtotime("{$year}-W{$week}+1")); //Returns the date of monday in week
+        if($from < $start_date) 
+
+            $from = $start_date;
+            $to = date("Y-m-d", strtotime("{$year}-W{$week}-6"));   //Returns the date of sunday in week
+        
+        if($to > $end_date) 
+            $to     = $end_date;
+            $period = new DatePeriod(new DateTime($from), new DateInterval('P1D'), new DateTime($to .'  +1 day'));
+            $days   = [];
+
+        foreach ($period as $date) {
+
+            $day['date']        = $date->format("d-m-Y");
+            $day['day']         = date('l', strtotime($date->format("d-m-Y")));
+            $day['programmes']  = null;
+            $day['attendance']  = $attendance;
+            $day['monetary']    = $monetary;
+            array_push($days,$day);
+        }
+        return $days;
+    } 
+
+    /**
+     * Delete an existing Report
+     *
+     * @param Request $request
+     * @param $listId
+     * @return \Illuminate\Http\JsonResponse
+     */
+
+    public function deleteReport($report_id) {
+
+        try {
+            DB::beginTransaction();
+
+            $report = Report::findOrFail($report_id)->delete();
+
+            if($report)
+            {
+
+            $response = [
+                'status'    => true,
+                'message'   => "Report deleted successfully."
+                ];
+                $responseCode = 200;
+            }
+            else
+            {
+
+               $response = [
+                'status'    => true,
+                'message'   => "No Report has been found."
+                ];
+                $responseCode = 404;  
+            }
+           
+        } catch (HttpBadRequestException $httpBadRequestException) {
+            $response = [
+                'status'    => false,
+                'error'     => $httpBadRequestException->getMessage()
+            ];
+            $responseCode = 400;
+        } catch (ClientException $clientException) {
+            DB::rollBack();
+
+            $response = [
+                'status'        => false,
+                'error'         => "Internal server error.",
+                'error_info'    => $clientException->getMessage()
+            ];
+            $responseCode = 500;
+        } catch (Exception $exception) {
+            DB::rollBack();
+
+            Log::error($exception->getMessage());
+
+            $response = [
+                'status'        => false,
+                'error'         => "Internal server error.",
+                'error_info'    => $exception->getMessage()
+            ];
+
+            $responseCode = 500;
+        } finally {
+            DB::commit();
+
+            unset($user);
+            unset($area);
+        }
+
+        return response()->json($response, $responseCode);
+    }
+
+    /**
+     * Filter Report depend on report id, month , year, and parish id
+     *
+     * @param Request $request
+     * @param $listId
+     * @return \Illuminate\Http\JsonResponse
+     */
+
+    public function updateReport(Request $request, $report_id) {
+        try {
+
+            DB::beginTransaction();
             
+             /*
+             * Validate mandatory fields
+             */
+            if ($report_id)
+
+                $report = Report::find($report_id);
+            else
+                throw new HttpBadRequestException("Report Id is required."); 
+
+            if ($request->has('user_id'))
+
+                $parish_id = $request->input('user_id');
+            else
+                throw new HttpBadRequestException("User Id is required.");
+
+            if ($request->has('progress_report'))
+
+                $progress_report = $request->input('progress_report');
+            else
+                throw new HttpBadRequestException("Progress report is required.");
+
+            if ($request->has('report_month'))
+
+                $report_month   = $request->input('report_month');
+            else
+                throw new HttpBadRequestException("Month is required.");
+
+            if ($request->has('report_year'))
+
+                $report_year    = $request->input('report_year');
+            else
+                throw new HttpBadRequestException("Year is required.");
             
+            $parish             = Parish::where('user_id',$parish_id)->whereNull('deleted_at')->first();
+
+            if($parish) {
+                if($report) {
+                    $report->report_month       = $report_month;
+                    $report->report_year        = $report_year;
+                    $report->progress_report    = $progress_report;
+                    $report->save();
+                } else {
+                   throw new HttpBadRequestException("Report not found."); 
+                }
+            } else {
+                throw new HttpBadRequestException("Parish not found.");
+            }
+            
+            $response = [
+                'status'        => true,
+                'message'       => "Report created successfully."
+            ];
+            $responseCode = 201;
            
         } catch (HttpBadRequestException $httpBadRequestException) {
                 $response = [
@@ -262,35 +478,6 @@ class ReportController extends Controller {
         }
 
         return response()->json($response, $responseCode);
-    }
-
-    /** Function to get no of week within a week for specified month and year **/
-
-    function getWeekDates($date, $start_date, $end_date,$attendance,$monetary){
-
-        $week   =  date('W', strtotime($date));
-        $year   =  date('Y', strtotime($date));
-        $from   = date("Y-m-d", strtotime("{$year}-W{$week}+1")); //Returns the date of monday in week
-        if($from < $start_date) 
-
-            $from = $start_date;
-            $to = date("Y-m-d", strtotime("{$year}-W{$week}-6"));   //Returns the date of sunday in week
-        
-        if($to > $end_date) 
-            $to     = $end_date;
-            $period = new DatePeriod(new DateTime($from), new DateInterval('P1D'), new DateTime($to .'  +1 day'));
-            $days   = [];
-
-        foreach ($period as $date) {
-
-            $day['date']        = $date->format("d-m-Y");
-            $day['day']         = date('l', strtotime($date->format("d-m-Y")));
-            $day['programmes']  = null;
-            $day['attendance']  = $attendance;
-            $day['monetary']    = $monetary;
-            array_push($days,$day);
-        }
-        return $days;
-    }  
+    } 
 
  }
