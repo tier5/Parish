d8fcbd4a79a26d0df5e6e57591c7482b1ad18e9f
Tier5 LLC <github@tier5.us>
2017-09-19 13:35:22 +0530

    report v1.2 released

--- app/Http/Controllers/Api/V1/ReportController.php
+++ app/Http/Controllers/Api/V1/ReportController.php
@@ -24,6 +24,9 @@ use Illuminate\Http\Request;
 use JWTAuth;
 use JWTAuthException;
 use Log;
+use DateTime;
+use DateInterval;
+use DatePeriod;
 
 class ReportController extends Controller {
 
@@ -157,9 +160,9 @@ class ReportController extends Controller {
             else
                 throw new HttpBadRequestException("Year is required.");
             
-            $parish         = Parish::find($parish_id);
+            $parish         = Parish::where('user_id',$parish_id)->first();
             
-            $fetchAllreport = Report::where('parish_id', $parish_id)
+            $fetchAllreport = Report::where('parish_id', $parish->id)
                                     ->where('report_month', $report_month)
                                     ->where('report_year', $report_year)
                                     ->first();
@@ -186,16 +189,56 @@ class ReportController extends Controller {
                 $monthly_total['attendance'] =$attendance;
                 $monthly_total['monetary'] =$monetary;
 
-                /*for($i= 1; $i<7; $i++)
-                {
-                    $days[$i]['date'] ='';
-                    $days[$i]['day'] ='';
-                    $days[$i]['programmes'] ='';
-                    $days[$i]['attendance'] =$attendance;
-                    $days[$i]['monetary'] =$monetary;
-                }*/
-                
+                $days = [];
+                for($i= 1; $i<=7; $i++){
 
+                   $day['date'] = null;
+                   $day['day'] = null;
+                   $day['programmes'] = null;
+                   $day['attendance'] = $attendance;
+                   $day['monetary'] = $monetary;
+
+                   array_push($days,$day);
+                }
+
+                $week = [];
+                for($k =1; $k<$no_week; $k++) {
+
+                    $week_total['attendance'] = $attendance;
+                    $week_total['monetary'] = $monetary;
+                    $setday['days'] = $days;
+                    array_push($week,$week_total);
+                    array_push($week,$setday);
+                }
+                
+               /* $res = $this->getWeekDays($report_month,$report_year);
+                
+                $o = '<table border="1">';
+                $o.= '<tr><th>Week</th><th>Monday</th><th>Tuesday</th><th>Wednesday</th><th>Thursday</th><th>Friday</th><th>Saturday</th><th>Sunday</th></tr>';
+                foreach ($res as $week => $dates) {
+                    $firstD = $dates[0];
+                    $lastD = $dates[count($dates)-1];
+
+                    $o.= "<tr>";
+                    $o.= "<td>" . $firstD->format('M d') . ' - ' . $lastD->format('M d') . "</td>";
+                    $N = $firstD->format('N');
+                    for ($i = 1; $i < $N; $i++) {
+                        $o.= "<td>-</td>";
+                    }
+                    foreach ($dates as $d) {
+                        $o.= "<td>" . $d->format('d.') . " / 0.00</td>";
+                            # for selected date do you magic
+                    }
+                    $N = $lastD->format('N');
+                    for ($i = $N; $i < 7; $i++) {
+                        $o.= "<td>-</td>";
+                    }
+                    $o.= "</tr>";
+                }
+                $o.= '</table>';
+                echo $o;
+                
+                dd('test');*/
                 $report["account_name"] =$parish->name;
                 $report["parish_id"] = $parish_id;
                 $report["parish_pastor"] = $parish->users->first_name." ".$parish->users->last_name;
@@ -204,10 +247,23 @@ class ReportController extends Controller {
                 $report["province_pastor"] =$parish->areas->zones->proviences->users->first_name." ".$parish->areas->zones->proviences->users->last_name;
                 $report["month"] =$report_month;
                 $report["year"] =$report_year;
+                $report["crucial_date"] =null;
                 $report['monthly_total'] = $monthly_total;
-
-
+                $report['weekly'] = $week;
             }
+
+
+            /*$signupdate='2017-09-01';
+            $signupweek=date("W",strtotime($signupdate));
+            $year=date("Y",strtotime($signupdate));
+            $currentweek = date("W");
+
+            for($i=$signupweek;$i<=$currentweek;$i++) {
+                $result=$this->getWeek($i,$year);
+                echo "Week:".$i." Start date:".$result['start']." End date:".$result['end']."<br>";
+            }
+
+            dd($no_week);*/
 
             $response = [
             'status'        => true,
@@ -269,6 +325,30 @@ class ReportController extends Controller {
            return ((52 + $end_week) - $start_week) + 1;
          }
         return ($end_week - $start_week) + 1;
+    }
+
+    function getWeek($week, $year) {
+      $dto = new DateTime();
+      $result['start'] = $dto->setISODate($year, $week, 0)->format('Y-m-d');
+      $result['end'] = $dto->setISODate($year, $week, 6)->format('Y-m-d');
+      return $result;
+    }
+
+    function getWeekDays($month, $year)
+    {
+        $p = new DatePeriod(
+            DateTime::createFromFormat('!Y-n-d', "$year-$month-01"),
+            new DateInterval('P1D'),
+            DateTime::createFromFormat('!Y-n-d', "$year-$month-01")->add(new DateInterval('P1M'))
+        );
+
+        $datesByWeek = array();
+        //dd($p);
+        foreach ($p as $d) {
+            
+            $dateByWeek[ $d->format('W') ][] = $d;
+        }
+        return $dateByWeek;
     }
 
  }
